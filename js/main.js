238483e361d9bcc726dc59c2a013a849b95f7470
Nathaniel Rubin <nateboop@gmail.com>
2015-02-04 00:52:20 -0500

    Going to try to rebuild nav myself

--- js/main.js
+++ js/main.js
@@ -3,4 +3,9 @@ $(document).ready(function() {
   $(".resize").on("click", function() {
     $(".content_wrapper").css("width", "200px");
   });
+
+  $(".nav-collapse a").on("click", function() {
+    $(".active").removeClass("active");
+    $(this).parent().addClass("acive");
+  });
 });
