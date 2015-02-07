ff43101bab9e6a26b9693a6977b37298b60adbd9
Nathaniel Rubin <nateboop@gmail.com>
2015-02-07 18:07:32 -0500

    Ok pushing up weverything

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
   setTimeout(function() {$(".me-section").addClass("visible");
-  }, 500);
+  $(".submit").addClass("submit-pressable");
+  }, 2000);
   $(".avatar").on("click", function() {
       //$( this ).effect( "bounce", "slow" );
       $( this ).effect( "slide", "slow" );
   });
+
+  $(".submit").addClass("submit-pressable");
 });
